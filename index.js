e28ef545fb061f14ae5a543d028fc8c7acf16a47
Cristian B <cris.bahena77@gmail.com>
2023-12-14 12:25:31 -0600

    added todays time, and date

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* global moment */
 console.log("Hello, world!")
+
 let datetime = moment().startOf("day").fromNow();
 console.log(datetime);
+
+let timeToday = moment().calendar();
+console.log(timeToday);
+
+let date = moment().format('MMMM Do YYYY, h:mm:ss a');                    
+console.log(date);
